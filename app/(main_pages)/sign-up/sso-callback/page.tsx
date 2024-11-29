df415c680d7beac992fd4e557ac8330a6999b873
Saaketh Kesireddy <saakethr.kesireddy@gmail.com>
2024-11-29 10:18:48 -0500

    fixed auth redirect callback

--- app/(main_pages)/sign-up/sso-callback/page.tsx
+++ app/(main_pages)/sign-up/sso-callback/page.tsx
@@ -4,5 +4,5 @@ export default function SSOCallback() {
   // Handle the redirect flow by rendering the
   // prebuilt AuthenticateWithRedirectCallback component.
   // This is the final step in the custom OAuth flow.
-  return <AuthenticateWithRedirectCallback />
+  return <AuthenticateWithRedirectCallback signInFallbackRedirectUrl={"/"} signUpForceRedirectUrl={"/"} />
 }
